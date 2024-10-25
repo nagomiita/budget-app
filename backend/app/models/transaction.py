5aff7f84ea86c7139cc18ed79543d4c47478cfa0
nagomiita <konnichiwatakeshi@gmail.com>
2024-10-25 09:02:48 +0900

    tttt

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -1,5 +1,19 @@
 from core.database import Base
-from sqlalchemy import Column, Date, Integer, Numeric, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, Numeric, String
+from sqlalchemy.orm import relationship
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)  # カテゴリ名
+    type = Column(String, nullable=False)  # タイプ名
+    color = Column(String, nullable=False)  # カテゴリカラー
+    icon_base64 = Column(String, nullable=True)  # Base64エンコードされたアイコンデータ
+
+    # Transactionテーブルとのリレーションシップ
+    transactions = relationship("Transaction", back_populates="category")
 
 
 class Transaction(Base):
@@ -10,6 +24,11 @@ class Transaction(Base):
     amount = Column(Numeric, nullable=False)
     content = Column(String, nullable=False)
     type = Column(String, nullable=False)  # income or expense
-    category = Column(String, nullable=False)  # カテゴリ（収入または支出）
     source = Column(String, nullable=True)  # データソースのタイプ
     transaction_type = Column(String, nullable=True)  # 'bank' or 'credit_card' など
+    category_id = Column(
+        Integer, ForeignKey("categories.id"), nullable=False
+    )  # カテゴリの外部キー
+
+    # Categoryテーブルとのリレーションシップ
+    category = relationship("Category", back_populates="transactions")
